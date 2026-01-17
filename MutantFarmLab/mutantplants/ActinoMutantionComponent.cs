fca8cd0a25e4bb8c14f8a050a5097ae1e2de9595
priy0315 <priy0315@hotmail.com>
2026-01-17 13:46:15 +0800

    修改辐光菌变异，移除光抗性组件导致崩溃问题，以及随植株状态控制光与辐射输出

--- MutantFarmLab/mutantplants/ActinoMutantionComponent.cs
+++ MutantFarmLab/mutantplants/ActinoMutantionComponent.cs
@@ -1,14 +1,23 @@
 ﻿using HarmonyLib;
+using Klei.AI;
 using PeterHan.PLib.Core;
+using System;
 using TUNING;
 using UnityEngine;
 using static LightColorMenu;
+using static STRINGS.CREATURES.STATUSITEMS;
 
 namespace MutantFarmLab.mutantplants
 {
     public class ActinoMutantionComponent: KMonoBehaviour
     {
         public Color lightColor = Color.green;
+        private Light2D light2D;
+        private RadiationEmitter radiationEmitter;
+        [MyCmpReq]
+        private WiltCondition _wiltCondition;
+        [MyCmpReq]
+        private Growing _growing;
         //public readonly EffectorValues POSITIVE_DECOR_EFFECT = DECOR.BONUS.TIER3;
         //public readonly EffectorValues NEGATIVE_DECOR_EFFECT = DECOR.PENALTY.TIER3;
         protected override void OnSpawn()
@@ -18,7 +27,17 @@ namespace MutantFarmLab.mutantplants
             var illumCom = gameObject.GetComponent<IlluminationVulnerable>();
             if(illumCom != null)
             {
-                DestroyImmediate(illumCom);
+                illumCom.prefersDarkness = false;
+                if(illumCom.LightIntensityThreshold != 0)
+                {
+                    PUtil.LogDebug($"[辐光菌][{illumCom.LightIntensityThreshold}] removing light requirement for " + gameObject.name);
+                    gameObject.GetComponent<Modifiers>().attributes.Add(
+                    new AttributeModifier(
+                        Db.Get().PlantAttributes.MinLightLux.Id,
+                        -illumCom.LightIntensityThreshold,
+                        "ActionMutation Symbiosis"
+                    ));
+                }
             }
             //TODO:移除气体需求组件
             var gasCom = gameObject.GetComponent<PressureVulnerable>();
@@ -30,7 +49,7 @@ namespace MutantFarmLab.mutantplants
             //挂载辐射源组件
             if (DlcManager.FeatureRadiationEnabled())
             {
-                RadiationEmitter radiationEmitter = gameObject.AddOrGet<RadiationEmitter>();
+                radiationEmitter = gameObject.AddOrGet<RadiationEmitter>();
                 radiationEmitter.emitType = RadiationEmitter.RadiationEmitterType.Constant;
                 radiationEmitter.radiusProportionalToRads = false;
                 radiationEmitter.emitRadiusX = 6;
@@ -42,7 +61,7 @@ namespace MutantFarmLab.mutantplants
             //挂载光照组件
             if (lightColor != Color.black)
             {
-                Light2D light2D = gameObject.AddOrGet<Light2D>();
+                light2D = gameObject.AddOrGet<Light2D>();
                 light2D.Color = lightColor;
                 light2D.overlayColour = LIGHT2D.LIGHTBUG_OVERLAYCOLOR;
                 light2D.Range = 5f;
@@ -52,16 +71,49 @@ namespace MutantFarmLab.mutantplants
                 light2D.shape = global::LightShape.Circle;
                 light2D.drawOverlay = true;
                 light2D.Lux = PlantMutationRegister.ACTINO_LIGHT_LUX;
+                
                 //gameObject.AddOrGet<LightSymbolTracker>().targetSymbol = "snapTo_light_locator";
             }
 
-            //DecorProvider decorProvider = gameObject.AddOrGet<DecorProvider>();
-            //decorProvider.SetValues(POSITIVE_DECOR_EFFECT);
-            //decorProvider.overrideName = name;
+            //GameHashes.TooHotWarning/GameHashes.TooColdWarning/GameHashes.Wilt/GameHashes.Grow/GameHashes.WiltRecover
+            Subscribe((int)GameHashes.TooHotWarning, (_)=> setActionActive(false));
+            Subscribe((int)GameHashes.TooColdWarning, (_) => setActionActive(false));
+            Subscribe((int)GameHashes.Grow, OnPlantGrowing);
+            Subscribe((int)GameHashes.Wilt, OnPlantStatusChanged);
+            Subscribe((int)GameHashes.WiltRecover, OnPlantStatusChanged);
+        }
 
-            //gameObject.AddOrGetDef<DecorPlantMonitor.Def>();
-            //prickleGrass.positive_decor_effect = this.POSITIVE_DECOR_EFFECT;
-            //prickleGrass.negative_decor_effect = this.NEGATIVE_DECOR_EFFECT;
+        private void OnPlantGrowing(object obj)
+        {
+            if(_growing != null && _growing.IsGrowing())
+            {
+                setActionActive(true);
+            }
+        }
+
+        private void OnPlantStatusChanged(object obj)
+        {
+            if (_wiltCondition == null) return;
+            if (_wiltCondition.IsWilting())
+            {
+                setActionActive(false);
+            }
+            else
+            {
+                setActionActive(true);
+            }
+        }
+        private void setActionActive(bool active)
+        {
+            bool canEmmit = true;
+            if (!active)
+            {
+                canEmmit = false;
+            }
+            if (light2D != null)
+                light2D.enabled = canEmmit;
+            if (radiationEmitter != null)
+                radiationEmitter.SetEmitting(canEmmit);
         }
     }
 
